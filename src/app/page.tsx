3d2c21a62c0b5d27fff9c128e7333c9fd2b66b9b
ddlmar <ddlmar13@gmail.com>
2025-11-24 20:51:32 -0300

    create a message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,13 @@
 export default function Home() {
-  return <h1>Home page</h1>;
+  return (
+    <div className="flex flex-col items-center h-screen justify-center">
+      <div className="-translate-y-1/4">
+        <h1>Olá, bem!</h1>
+        <img
+          src={"https://media.tenor.com/QZ-uE1CvMmcAAAAM/kiss.gif"}
+          className="size-64"
+        />
+      </div>
+    </div>
+  );
 }
